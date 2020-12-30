f39b687349485dc533e041c372be8c557cff5026
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-30 18:26:01 -0500

    Use camel case

--- src/Page01/Problem01.hs
+++ src/Page01/Problem01.hs
@@ -1,9 +1,10 @@
-module Page01.Problem01(
-  solution
-) where
+module Page01.Problem01
+  ( solution,
+  )
+where
 
 divides a b = b `mod` a == 0
 
-sum_multiples_of_3_and_5 n = sum [k | k <- [1 .. n], 3 `divides` k, 5 `divides` k]
+sumMultiplesOf3And5 n = sum [k | k <- [1 .. n], 3 `divides` k, 5 `divides` k]
 
-solution = sum_multiples_of_3_and_5 1000
+solution = sumMultiplesOf3And5 1000
