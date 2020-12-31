69259281705ebe8dcba8519bfcbfefd50b1859ae
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-30 23:24:32 -0500

    Use the classic Fibonacci definition in Problem 2 (#3)
    * Add new modules to the .cabal file
    
    * Simplify binaryRecurrence

--- src/Math/Combinatorics.hs
+++ src/Math/Combinatorics.hs
@@ -1,25 +1,16 @@
 module Math.Combinatorics
   (
+    binaryRecurrence,
     fibonacci,
-    binaryRecurrence
   )
 where
-
-binaryRecurrenceRecursive :: (a -> a -> a) -> [a] -> [a]
-binaryRecurrenceRecursive f []  = error "empty list"
-binaryRecurrenceRecursive f [x] = error "singleton list"
-binaryRecurrenceRecursive f xs  = [next] ++ binaryRecurrenceRecursive f [last xs, next]
-  where next = f (last xs) (last $ init xs)
-
--- binaryRecurrence (+) 1 1
--- = [1, 1] binaryRecurrenceRecursive (+) [1, 1]
--- = [1, 1] ++ [2] ++ binaryRecurrenceRecursive (+) [1, 2]
--- = [1, 1] ++ [2] ++ [3] ++ binaryRecurrenceRecursive (+) [2, 3]
 
 -- | Define a recurrence relation using a binary function and two initial values.
 -- The recurrence will follow the pattern `a[n] = f a[n - 1] a[n - 2]`.
 binaryRecurrence :: (a -> a -> a) -> a -> a -> [a]
-binaryRecurrence f x y = [x, y] ++ binaryRecurrenceRecursive f [x, y]
+binaryRecurrence f x y = x : y : zipWith f self (tail self)
+  where self = binaryRecurrence f x y
 
 -- | The infinite sequence of Fibonacci numbers.
+fibonacci :: Num a => [a]
 fibonacci = binaryRecurrence (+) 1 1
