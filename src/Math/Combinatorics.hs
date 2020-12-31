ea8d40651a95ce323d9e1514581a22a1bb701701
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-30 22:05:01 -0500

    Use more natural naming

--- src/Math/Combinatorics.hs
+++ src/Math/Combinatorics.hs
@@ -1,25 +1,25 @@
 module Math.Combinatorics
   (
     fibonacci,
-    recurrenceBinary
+    binaryRecurrence
   )
 where
 
-recurrenceBinaryRecursive :: (a -> a -> a) -> [a] -> [a]
-recurrenceBinaryRecursive f []  = error "empty list"
-recurrenceBinaryRecursive f [x] = error "singleton list"
-recurrenceBinaryRecursive f xs  = [next] ++ recurrenceBinaryRecursive f [last xs, next]
+binaryRecurrenceRecursive :: (a -> a -> a) -> [a] -> [a]
+binaryRecurrenceRecursive f []  = error "empty list"
+binaryRecurrenceRecursive f [x] = error "singleton list"
+binaryRecurrenceRecursive f xs  = [next] ++ binaryRecurrenceRecursive f [last xs, next]
   where next = f (last xs) (last $ init xs)
 
--- recurrenceBinary (+) 1 1
--- = [1, 1] recurrenceBinaryRecursive (+) [1, 1]
--- = [1, 1] ++ [2] ++ recurrenceBinaryRecursive (+) [1, 2]
--- = [1, 1] ++ [2] ++ [3] ++ recurrenceBinaryRecursive (+) [2, 3]
+-- binaryRecurrence (+) 1 1
+-- = [1, 1] binaryRecurrenceRecursive (+) [1, 1]
+-- = [1, 1] ++ [2] ++ binaryRecurrenceRecursive (+) [1, 2]
+-- = [1, 1] ++ [2] ++ [3] ++ binaryRecurrenceRecursive (+) [2, 3]
 
 -- | Define a recurrence relation using a binary function and two initial values.
 -- The recurrence will follow the pattern `a[n] = f a[n - 1] a[n - 2]`.
-recurrenceBinary :: (a -> a -> a) -> a -> a -> [a]
-recurrenceBinary f x y = [x, y] ++ recurrenceBinaryRecursive f [x, y]
+binaryRecurrence :: (a -> a -> a) -> a -> a -> [a]
+binaryRecurrence f x y = [x, y] ++ binaryRecurrenceRecursive f [x, y]
 
 -- | The infinite sequence of Fibonacci numbers.
-fibonacci = recurrenceBinary (+) 1 1
+fibonacci = binaryRecurrence (+) 1 1
