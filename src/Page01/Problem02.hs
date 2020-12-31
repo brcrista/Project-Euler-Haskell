03abf600e54b95eafdde118b2976a760ab5593f2
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-30 21:53:30 -0500

    Problem 2 solution

---
+++ src/Page01/Problem02.hs
@@ -0,0 +1,28 @@
+module Page01.Problem02
+  (
+    solution
+  )
+where
+
+recurrenceBinaryRecursive :: (a -> a -> a) -> [a] -> [a]
+recurrenceBinaryRecursive f []  = error "empty list"
+recurrenceBinaryRecursive f [x] = error "singleton list"
+recurrenceBinaryRecursive f xs  = [next] ++ recurrenceBinaryRecursive f [last xs, next]
+  where next = f (last xs) (last $ init xs)
+
+-- recurrenceBinary (+) 1 1
+-- = [1, 1] recurrenceBinaryRecursive (+) [1, 1]
+-- = [1, 1] ++ [2] ++ recurrenceBinaryRecursive (+) [1, 2]
+-- = [1, 1] ++ [2] ++ [3] ++ recurrenceBinaryRecursive (+) [2, 3]
+
+-- | Define a recurrence relation using a binary function and two initial values.
+-- The recurrence will follow the pattern `a[n] = f a[n - 1] a[n - 2]`.
+recurrenceBinary :: (a -> a -> a) -> a -> a -> [a]
+recurrenceBinary f x y = [x, y] ++ recurrenceBinaryRecursive f [x, y]
+
+-- | The infinite sequence of Fibonacci numbers.
+fibonacci = recurrenceBinary (+) 1 1
+
+sumEvenFibonacciNumbers n = sum [fib | fib <- takeWhile (<= n) fibonacci, even fib]
+
+solution = sumEvenFibonacciNumbers 4000000
