433a36d0f2edf17150a75154800f83a1fba9c007
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-02 16:29:52 -0500

    Get tests running

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Test.HUnit
 import qualified Tests.Page01.Problem01
 import qualified Tests.Page01.Problem02
 
-tests = TestGroup "Solution tests" [Tests.Page01.Problem01.testSolution, Tests.Page01.Problem02.testSolution]
+tests = TestList [Tests.Page01.Problem01.testSolution, Tests.Page01.Problem02.testSolution]
+
+main = runTestTT tests
--- test/Tests/Page01/Problem02.hs
+++ test/Tests/Page01/Problem02.hs
@@ -3,4 +3,4 @@ module Tests.Page01.Problem02 where
 import Page01.Problem02
 import Test.HUnit
 
-testSolution = TestCase (assertEqual "Checking the solution:", 4613732, solution)
+testSolution = TestCase (assertEqual "Checking the solution:" 4613732 solution)
