701b3d11ed6611fff12ab04f82eb0b2030336295
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-21 18:46:00 -0500

    Add Math.Combinatorics tests (failing)

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -2,12 +2,14 @@ module Main where
 
 import Test.HUnit
 import qualified Tests.Math
+import qualified Tests.Math.Combinatorics
 import qualified Tests.Page01.Problem01
 import qualified Tests.Page01.Problem02
 
 tests = TestList
   [
     Tests.Math.tests,
+    Tests.Math.Combinatorics.tests,
     Tests.Page01.Problem01.tests,
     Tests.Page01.Problem02.tests
   ]
---
+++ test/Tests/Math/Combinatorics.hs
@@ -0,0 +1,15 @@
+module Tests.Math.Combinatorics where
+
+import Math.Combinatorics
+import Test.HUnit
+
+lucasNumbers = binaryRecurrence (+) 2 1
+
+testBinaryRecurrence = TestList $ map TestCase
+  [
+    assertEqual "First 5 Lucas numbers" [2, 1, 3, 4, 7] (take 5 lucasNumbers),
+    assertEqual "Applies the function in the correct order" [0, 1, 1, 0, -1, -1, 0, 1]   (take 8 $ binaryRecurrence (-)      0 1),
+    assertEqual "Applies the function in the correct order" [0, 1, -1, 2, -3, 5, -8, 13] (take 8 $ binaryRecurrence subtract 0 1)
+  ]
+
+tests = TestList [testBinaryRecurrence]
