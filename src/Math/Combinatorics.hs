2f79a5e0a30ea2ee3a414aecee5c3c3d665580a5
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-21 18:46:07 -0500

    Fix bug in binaryRecurrence

--- src/Math/Combinatorics.hs
+++ src/Math/Combinatorics.hs
@@ -8,7 +8,7 @@ where
 -- | Define a recurrence relation using a binary function and two initial values.
 -- The recurrence will follow the pattern `a[n] = f a[n - 1] a[n - 2]`.
 binaryRecurrence :: (a -> a -> a) -> a -> a -> [a]
-binaryRecurrence f x y = x : y : zipWith f self (tail self)
+binaryRecurrence f x y = x : y : zipWith f (tail self) self
   where self = binaryRecurrence f x y
 
 -- | The infinite sequence of Fibonacci numbers.
