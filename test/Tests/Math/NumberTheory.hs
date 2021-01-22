4b11841bff592afefff1166b603febdd908c4e29
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-21 19:45:44 -0500

    add tests for other functions

--- test/Tests/Math/NumberTheory.hs
+++ test/Tests/Math/NumberTheory.hs
@@ -11,4 +11,22 @@ testDivides = TestList $ map TestCase
     assertEqual "" True  (2  `divides` 0)
   ]
 
-tests = TestList [testDivides]
+testIsPrime = TestList $ map TestCase
+  [
+    assertEqual "" False (isPrime 0),
+    assertEqual "" False (isPrime 1),
+    assertEqual "" True  (all isPrime primes),
+    assertEqual "" False (any isPrime composites)
+  ]
+  where
+    primes     = [2, 3, 5, 7, 11, 13, 41]
+    composites = [4, 6, 12, 100, 10000]
+
+testFactors = TestList $ map TestCase
+  [
+    assertEqual "" [1]          (factors 1),
+    assertEqual "" [1, 2]       (factors 2),
+    assertEqual "" [1, 2, 4, 8] (factors 8)
+  ]
+
+tests = TestList [testDivides, testIsPrime, testFactors]
