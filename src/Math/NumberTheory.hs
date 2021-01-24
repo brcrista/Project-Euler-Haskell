532885fcd34e11908eb8be28a59fc72efe14b0c6
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-23 19:00:11 -0500

    Use Numeric.Natural in Math.NumberTheory

--- src/Math/NumberTheory.hs
+++ src/Math/NumberTheory.hs
@@ -7,20 +7,21 @@ module Math.NumberTheory
 where
 
 import Data.List (nub, sort)
+import Numeric.Natural
 
 -- | Whether `b` is a multiple of `a`.
 divides :: Integral a => a -> a -> Bool
 divides a b = b `mod` a == 0
 
 -- | The pairs of factors of a natural number `n`.
-factorPairs :: Integral a => a -> [(a, a)]
+factorPairs :: Natural -> [(Natural, Natural)]
 factorPairs n = [(x, n `div` x) | x <- [1 .. floorSqrt n], x `divides` n]
   where floorSqrt = floor . sqrt . fromIntegral
 
 -- | The factors of a natural number `n`.
-factors :: Integral a => a -> [a]
+factors :: Natural -> [Natural]
 factors n = sort . nub $ concat [[a, b] | (a, b) <- factorPairs n]
 
 -- | Whether a natural number `n` is prime.
-isPrime :: Integral a => a -> Bool
+isPrime :: Natural -> Bool
 isPrime n = n > 1 && factors n == [1, n]
