4ad010eb7e59635d30afa4f7f90d5bb7632141ff
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-02-06 14:58:35 -0500

    Remove redundant brackets

--- test/Tests/Math.hs
+++ test/Tests/Math.hs
@@ -6,32 +6,32 @@ import Test.Tasty.HUnit
 
 testEmptyRange = testGroup "sumRange empty range"
   [
-    testCase "Sum of an empty range" $ (sumRange 0 (-1) 1)  @?= 0,
-    testCase "Sum of an empty range" $ (sumRange 1 0 1)     @?= 0,
-    testCase "Sum of an empty range" $ (sumRange 1 10 (-1)) @?= 0
+    testCase "Sum of an empty range" $ sumRange 0 (-1) 1  @?= 0,
+    testCase "Sum of an empty range" $ sumRange 1 0 1     @?= 0,
+    testCase "Sum of an empty range" $ sumRange 1 10 (-1) @?= 0
   ]
 
 testSingletonRange = testGroup "sumRange singleton range"
   [
-    testCase "Sum of a singleton range" $ (sumRange 0 0 1) @?= 0,
-    testCase "Sum of a singleton range" $ (sumRange 1 1 1) @?= 1,
-    testCase "Sum of a singleton range" $ (sumRange 2 2 1) @?= 2
+    testCase "Sum of a singleton range" $ sumRange 0 0 1 @?= 0,
+    testCase "Sum of a singleton range" $ sumRange 1 1 1 @?= 1,
+    testCase "Sum of a singleton range" $ sumRange 2 2 1 @?= 2
   ]
 
 testNaturals = testGroup "sumRange natural numbers"
   [
-    testCase "Sum of a range of natural numbers" $ (sumRange 0 10 1)  @?= 55,
-    testCase "Sum of a range of natural numbers" $ (sumRange 0 30 3)  @?= 165,
-    testCase "Sum of a range of natural numbers" $ (sumRange 10 20 1) @?= 165,
-    testCase "Sum of a range of natural numbers" $ (sumRange 9 30 3)  @?= 156
+    testCase "Sum of a range of natural numbers" $ sumRange 0 10 1  @?= 55,
+    testCase "Sum of a range of natural numbers" $ sumRange 0 30 3  @?= 165,
+    testCase "Sum of a range of natural numbers" $ sumRange 10 20 1 @?= 165,
+    testCase "Sum of a range of natural numbers" $ sumRange 9 30 3  @?= 156
   ]
 
 testNegative = testGroup "sumRange negative numbers"
   [
-    testCase "Sum of a range with negative numbers" $ (sumRange (-1) (-10) (-1)) @?= (-55),
-    testCase "Sum of a range with negative numbers" $ (sumRange (-9) (-30) (-3)) @?= (-156),
-    testCase "Sum of a range with negative numbers" $ (sumRange (-5) 5 1) @?= 0,
-    testCase "Sum of a range with negative numbers" $ (sumRange (-5) 6 1) @?= 6
+    testCase "Sum of a range with negative numbers" $ sumRange (-1) (-10) (-1) @?= (-55),
+    testCase "Sum of a range with negative numbers" $ sumRange (-9) (-30) (-3) @?= (-156),
+    testCase "Sum of a range with negative numbers" $ sumRange (-5) 5 1 @?= 0,
+    testCase "Sum of a range with negative numbers" $ sumRange (-5) 6 1 @?= 6
   ]
 
 tests = testGroup "Math" [testEmptyRange, testSingletonRange, testNaturals, testNegative]
--- test/Tests/Math/Combinatorics.hs
+++ test/Tests/Math/Combinatorics.hs
@@ -8,14 +8,14 @@ lucasNumbers = binaryRecurrence (+) 2 1
 
 testBinaryRecurrence = testGroup "binaryRecurrence"
   [
-    testCase "First 5 Lucas numbers" $ (take 5 lucasNumbers) @?= [2, 1, 3, 4, 7],
-    testCase "Applies the function in the correct order" $ (take 8 $ binaryRecurrence (-) 0 1) @?= [0, 1, 1, 0, -1, -1, 0, 1],
-    testCase "Applies the function in the correct order" $ (take 8 $ binaryRecurrence subtract 0 1) @?= [0, 1, -1, 2, -3, 5, -8, 13]
+    testCase "First 5 Lucas numbers" $ take 5 lucasNumbers @?= [2, 1, 3, 4, 7],
+    testCase "Applies the function in the correct order" $ take 8 (binaryRecurrence (-) 0 1) @?= [0, 1, 1, 0, -1, -1, 0, 1],
+    testCase "Applies the function in the correct order" $ take 8 (binaryRecurrence subtract 0 1) @?= [0, 1, -1, 2, -3, 5, -8, 13]
   ]
 
 testFibonacci = testGroup "fibonacci"
   [
-    testCase "First 7 Fibonacci numbers" $ (take 7 fibonacci) @?= [0, 1, 1, 2, 3, 5, 8]
+    testCase "First 7 Fibonacci numbers" $ take 7 fibonacci @?= [0, 1, 1, 2, 3, 5, 8]
   ]
 
 tests = testGroup "Math.Combinatorics" [testBinaryRecurrence, testFibonacci]
