52768581538b12bb230a8729db8460300805f0a9
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-01 15:55:50 -0500

    Add Problem 8 test

--- test/Tests/Page01/Problem08.hs
+++ test/Tests/Page01/Problem08.hs
@@ -1,8 +1,12 @@
 module Tests.Page01.Problem08 where
 
 import Page01.Problem08
-import Test.Tasty.HUnit
+import Test.Tasty (TestTree)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 unit_largestProductInSeries = largestProductInSeries 13 (replicate 13 '1') @?= 1
 
--- unit_problem_7 = solution @?= 104743
+test_problem_8 :: IO TestTree
+test_problem_8 = do
+  actual <- solution
+  return $ testCase "Problem 8" $ actual @?= 23514624000
