1a81edac4a05e044a3bdb2944e855e8254c50f42
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-02 09:27:57 -0500

    Add test

--- src/Page01/Problem09.hs
+++ src/Page01/Problem09.hs
@@ -1,7 +1,7 @@
 {-|
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which a^2 + b^2 = c^2
 
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product a * b * c.
@@ -9,17 +9,21 @@ Find the product a * b * c.
 
 module Page01.Problem09
   (
-    pythagoreanSumEquals,
+    isPythagorean,
     solution
   )
 where
 
 import Numeric.Natural (Natural)
 
+isPythagorean :: Natural -> Natural -> Natural -> Bool
+-- ^2 is just as fast as * in Haskell
+isPythagorean a b c = a^2 + b^2 == c^2
+
 pythagoreanTriples :: [(Natural, Natural, Natural)]
 pythagoreanTriples =
   [
-    (a, b, c) | c <- [3 ..], b <- [2 .. c], a <- [1 .. b], (a * a) + (b * b) == (c * c)
+    (a, b, c) | c <- [3 ..], b <- [2 .. c], a <- [1 .. b], isPythagorean a b c
   ]
 
 sum3Equals :: (Eq a, Num a) => a -> (a, a, a) -> Bool
