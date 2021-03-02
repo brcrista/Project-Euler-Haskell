fdf955385c143875d6d51db6d7831f1a03241505
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-02 14:14:47 -0500

    Simpler but still slow solution

--- src/Math/NumberTheory.hs
+++ src/Math/NumberTheory.hs
@@ -32,34 +32,17 @@ factors n = sort . nub $ factorPairs n >>= \ (a, b) -> [a, b]
 isPrime :: Natural -> Bool
 isPrime n = n > 1 && factors n == [1, n]
 
--- Note: the lift of left elements is kept in reverse
-newtype ListZipper a = ListZipper { focus :: ([a], [a]) } deriving Show
-
-start :: [a] -> ListZipper a
-start xs = ListZipper ([], xs)
-
-next :: ListZipper a -> ListZipper a
-next (ListZipper (_, [])) = error "end of list"
-next (ListZipper (xs, y : ys)) = ListZipper (y : xs, ys)
-
 -- | All primes less than a natural number `n`, computed with the Sieve of Eratosthenes.
 eratosthenes :: Natural -> [Natural]
--- eratosthenes n = mask naturals $ eratosthenesRecursive $ start maybePrimes
-eratosthenes n = mask naturals $ eratosthenesRecursive $ start maybePrimes
+eratosthenes n = mask naturals $ eratosthenesRecursive 2 maybePrimes
   where
     maybePrimes = [False, False] ++ [True | _ <- [2 .. n - 1]]
     mask xs = map fst . filter snd . zip xs
 
-eratosthenesRecursive :: ListZipper Bool -> [Bool]
-eratosthenesRecursive (ListZipper (definitelyPrimes, [])) = reverse definitelyPrimes
-eratosthenesRecursive zipper@(ListZipper (definitelyPrimes, ys@(stillPrime : maybePrimes))) =
-  let
-    i = length definitelyPrimes
-    -- Re-index; maybePrimes element 0 = i + 1
-    newTail = [if i `divides` (j + i + 1) then False else x | (j, x) <- zip [0 ..] maybePrimes]
-    replaceTail (ListZipper (xs, ys)) = ListZipper (xs, newTail)
-  in
-  if stillPrime
-  then eratosthenesRecursive $ replaceTail $ next zipper
-  else eratosthenesRecursive $ next zipper
-  -- unprimeMultiples = [2 * i, 3 * i .. n]
+eratosthenesRecursive :: Int -> [Bool] -> [Bool]
+eratosthenesRecursive i maybePrimes
+  | i >= length maybePrimes = maybePrimes
+  | otherwise = eratosthenesRecursive (i + 1) $
+    if maybePrimes !! i
+    then zipWith (\ j x -> if j > i && i `divides` j then False else x) [0 ..] maybePrimes
+    else maybePrimes
