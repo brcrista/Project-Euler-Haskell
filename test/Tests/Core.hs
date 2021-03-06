317e5d48dd9959683e230e38aa6707263f90dea5
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-05 21:13:14 -0500

    Data.Void

--- test/Tests/Core.hs
+++ test/Tests/Core.hs
@@ -1,6 +1,7 @@
 module Tests.Core where
 
 import Core
+import Data.Void
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -9,7 +10,7 @@ test_consecutives = testGroup "consecutives"
     testCase "0"          $ consecutives 0 [1 .. 4] @?= [],
     testCase "1"          $ consecutives 1 [1 .. 4] @?= [[1], [2], [3], [4]],
     testCase "2"          $ consecutives 2 [1 .. 4] @?= [[1, 2], [2, 3], [3, 4]],
-    testCase "Empty list" $ consecutives 3 []       @?= ([] :: [[Int]])
+    testCase "Empty list" $ consecutives 3 []       @?= ([] :: [[Void]])
   ]
 
 test_indices = testGroup "indices"
@@ -20,7 +21,7 @@ test_indices = testGroup "indices"
 
 test_slice = testGroup "slice"
   [
-    testCase "Empty list"          $ slice 0 100 []         @?= ([] :: [Int]),
+    testCase "Empty list"          $ slice 0 100 []         @?= ([] :: [Void]),
     testCase "Start out of bounds" $ slice (-1) 1 [0, 1, 2] @?= [0, 1],
     testCase "End out of bounds"   $ slice 1 10 [0, 1, 2]   @?= [1, 2],
     testCase "Slice beginning"     $ slice 0 2 [1 .. 5]     @?= [1, 2, 3],
