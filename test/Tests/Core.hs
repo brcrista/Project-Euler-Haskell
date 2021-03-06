db7266cbc0bdcc8912a425b64bb4cd5a6f678839
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-05 21:19:48 -0500

    add another test

--- test/Tests/Core.hs
+++ test/Tests/Core.hs
@@ -21,12 +21,13 @@ test_indices = testGroup "indices"
 
 test_slice = testGroup "slice"
   [
-    testCase "Empty list"          $ slice 0 100 []         @?= ([] :: [Void]),
-    testCase "Start out of bounds" $ slice (-1) 1 [0, 1, 2] @?= [0, 1],
-    testCase "End out of bounds"   $ slice 1 10 [0, 1, 2]   @?= [1, 2],
-    testCase "Slice beginning"     $ slice 0 2 [1 .. 5]     @?= [1, 2, 3],
-    testCase "Slice middle"        $ slice 2 3 [1 .. 5]     @?= [3, 4],
-    testCase "Slice end"           $ slice 3 4 [1 .. 5]     @?= [4, 5],
-    testCase "Singleton slice"     $ slice 2 2 [1 .. 5]     @?= [3],
-    testCase "Empty slice"         $ slice 4 3 [1 .. 5]     @?= []
+    testCase "Empty list"          $ slice 0 100 []            @?= ([] :: [Void]),
+    testCase "Start out of bounds" $ slice (-1) 1 [0, 1, 2]    @?= [0, 1],
+    testCase "End out of bounds"   $ slice 1 10 [0, 1, 2]      @?= [1, 2],
+    testCase "Start, end negative" $ slice (-4) (-1) [0, 1, 2] @?= [],
+    testCase "Slice beginning"     $ slice 0 2 [1 .. 5]        @?= [1, 2, 3],
+    testCase "Slice middle"        $ slice 2 3 [1 .. 5]        @?= [3, 4],
+    testCase "Slice end"           $ slice 3 4 [1 .. 5]        @?= [4, 5],
+    testCase "Singleton slice"     $ slice 2 2 [1 .. 5]        @?= [3],
+    testCase "Empty slice"         $ slice 4 3 [1 .. 5]        @?= []
   ]
