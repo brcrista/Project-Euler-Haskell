08bd7ac22da767d78c2a51c070a575998d4ee3a0
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-27 12:56:21 -0400

    Problem 19 code complete

--- src/Page01/Problem19.hs
+++ src/Page01/Problem19.hs
@@ -19,6 +19,10 @@ module Page01.Problem19
   )
 where
 
+import Data.Bool (bool)
+import Data.Function ((&))
+import Math.NumberTheory (divides)
+
 data Day =
   Monday
   | Tuesday
@@ -27,7 +31,7 @@ data Day =
   | Friday
   | Saturday
   | Sunday
-  deriving (Show, Enum)
+  deriving (Show, Enum, Eq)
 
 data Month =
   January
@@ -42,12 +46,41 @@ data Month =
   | October
   | November
   | December
-  deriving (Show, Enum)
+  deriving (Show, Enum, Eq)
 
 type Year = Int
 
+isLeapYear :: Year -> Bool
+isLeapYear year = 4 `divides` year && (400 `divides` year || not (100 `divides` year))
+
+daysInMonth :: Integral a => Month -> Year -> a
+daysInMonth January   _    = 31
+daysInMonth February  year = if isLeapYear year then 29 else 28
+daysInMonth March     _    = 31
+daysInMonth April     _    = 30
+daysInMonth May       _    = 31
+daysInMonth June      _    = 30
+daysInMonth July      _    = 31
+daysInMonth August    _    = 31
+daysInMonth September _    = 30
+daysInMonth October   _    = 31
+daysInMonth November  _    = 30
+daysInMonth December  _    = 31
+
+daysInYear :: Integral a => Year -> a
+daysInYear = bool 365 366 . isLeapYear
+
+-- | The day of the week `daysElapsed` after `day`.
+dayMod :: Day -> Int -> Day
+dayMod day daysElapsed = toEnum $ (fromEnum day + daysElapsed) `mod` 7
+
+firstDaysOfMonths :: [Day]
+firstDaysOfMonths = (,) <$> [January .. December] <*> [1901 .. 2000] & map (uncurry daysInMonth) & scanl dayMod firstDayOf1901
+  where
+    firstDayOf1901 = dayMod Monday $ daysInYear 1900 -- 1 Jan 1900 was a Monday
+
 countSundaysOnFirstOfMonth :: Int
-countSundaysOnFirstOfMonth = undefined
+countSundaysOnFirstOfMonth = length . filter (== Sunday) $ firstDaysOfMonths
 
 solution :: Int
 solution = countSundaysOnFirstOfMonth
